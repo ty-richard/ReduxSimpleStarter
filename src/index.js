1a800a83aa883567b888a3afa3665beea55615f3
Ty Richard <tyrichard2@icloud.com>
2017-10-12 18:30:08 -0600

    youtube clone

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+import YTSearch from "youtube-api-search"
+import _ from "lodash"
+import SearchBar from "./components/search_bar"
+import VideoList from "./components/video_list"
+import VideoDetail from "./components/video_detail"
+const API_KEY = "AIzaSyC2WuoFZq8z5DmmgupnIKSHmPXnUUUQgME"
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//create a new component
+//this component produces html
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { 
+      videos: [],
+      selectedVideo: null 
+    }
+    this.videoSearch("kanye West")
+  }
+  
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+    })
+  }
+
+  render()  {
+    const videoSearch = _.debounce((term) => { this.videoSearch (term) }, 300)
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo } />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    )
+  }
+}
+
+//take this component generated in the html
+
+ReactDOM.render(<App />, document.querySelector(".container"))
